cd812b3e2fe77f97c532269cbcdb432dbd5e46b1
mostafa <m.shakoori.74@gmail.com>
2020-06-22 13:49:22 +0430

    render_template added for index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>WELCOM DEAR</h1>'
+    return render_template('blog/index.html')
 
 
 if __name__ == '__main__':
