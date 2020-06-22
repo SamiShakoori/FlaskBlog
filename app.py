2dceafc86428fb845d64b38c944aea1dd9f28477
mostafa <m.shakoori.74@gmail.com>
2020-06-22 17:08:29 +0430

    registering config and migrate(db) and blueprint(users) in app

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Development
 
 
 app = Flask(__name__)
+app.config.from_object(Development)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
 def index():
     return render_template('blog/index.html')
+
+
+from mod_users import users
+
+app.register_blueprint(users)
 
 
 if __name__ == '__main__':
